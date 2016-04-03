149f2524acecd84afd873e8bc59258cbf40e8db6
janhridel <hridel@gmail.com>
2016-04-03 15:20:48 +0200

    dedicnost priklad polymorfismus

---
+++ Prednasky/04/02_dedicnost_polymorfismus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorfismus
+{
+    class Vlak
+    {
+        public void VypisNevirtualni() { Console.WriteLine("Vlak"); }
+        public virtual void VypisVirtualni() { Console.WriteLine("Vlak"); }
+    }
+    class NakladniVlak : Vlak
+    {
+        public new void VypisNevirtualni()
+        { Console.WriteLine("Nakladní vlak"); }
+        public override void VypisVirtualni()
+        { Console.WriteLine("Nakladní vlak"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vlak vlak1 = new Vlak();
+            Vlak vlak2 = new NakladniVlak();
+            vlak1.VypisNevirtualni();
+            vlak2.VypisNevirtualni();
+            vlak1.VypisVirtualni();
+            vlak2.VypisVirtualni();
+            Console.ReadKey();
+        }
+    }
+
+}
