7d7c26b22d6d62b9327277c75a52cc1a27439849
janhridel <hridel@gmail.com>
2016-04-17 17:51:56 +0200

    interface neviretualni metody

---
+++ Prednasky/05/06_interface_nevirtualni_metody.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contmp
+{
+    interface IVypisovací
+    {
+        void Vypis();
+    }
+    class A : IVypisovací
+    {
+        public void Vypis()
+        {
+            Console.WriteLine("Výpis údajů třídy A");
+        }
+    }
+    class B : A
+    {
+        public new void Vypis()
+        {
+            Console.WriteLine("Výpis údajů třídy B");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            A b = new B();
+            IVypisovací ia = a;
+            IVypisovací ib = b;
+            a.Vypis();
+            b.Vypis();
+            ia.Vypis();
+            ib.Vypis();
+            Console.ReadKey();
+        }
+    } 
+}
